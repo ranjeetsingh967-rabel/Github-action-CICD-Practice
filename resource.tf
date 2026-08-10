95248bf1a4047af3119af31259aa6255a7d150f3
Ranjeetsingh967-rabel <ranjeetsingh967@gmail.com>
2026-08-10 18:18:53 +0530

    new added

--- resource.tf
+++ resource.tf
@@ -2,3 +2,10 @@ resource "azurerm_resource_group" "rg1" {
   name     = "github-rg"
   location = "centralindia"
 }
+resource "azurerm_storage_account" "sa1" {
+  name                     = "githubsa"
+  resource_group_name      = azurerm_resource_group.rg1.name
+  location                 = azurerm_resource_group.rg1.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
